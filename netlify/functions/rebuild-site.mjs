87c1fc04626ebc08bdaee7c3a1ddf0687272b9d8
Dain Saint <dain@cipherprime.com>
2026-09-04 11:25:14 -0400

    update

--- netlify/functions/rebuild-site.mjs
+++ netlify/functions/rebuild-site.mjs
@@ -9,7 +9,7 @@ async function rebuildSite(triggerTitle) {
 export default async (req) => {
   const { next_run } = await req.json();
 
-  await rebuildSite("Daily rebuild");
+  await rebuildSite("Hourly rebuild");
   console.log("Received event! Next invocation at:", next_run);
 };
 
